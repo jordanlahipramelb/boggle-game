2fcea7003ecdb828422df7214a96be00a88d8854
jordanlahipramelb <jordan.lahipramelb@gmail.com>
2021-05-09 15:51:41 -0700

    added tests

--- test.py
+++ test.py
@@ -8,3 +8,14 @@ class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
+    def test_homepage(self):
+        """Test if the homepage is set up"""
+        with app.test_client() as client:
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("board", session)
+            self.assertIsNone(session.get("high_score"))
+            self.assertIn("Your high score is <b>0!</b>", html)
+            self.assertIsNone(session.get("num_plays"))
